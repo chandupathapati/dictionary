e1eadf94b207cd97cd8e16cb3148679d701bc144
CP068852 <pchandrashakervarmav@gmail.com>
2021-05-24 20:23:34 -0400

    Create project setup and add required files

---
+++ src/main/java/com/spreetail/dictionary/DictionaryConsoleApp.java
@@ -0,0 +1,10 @@
+package com.spreetail.dictionary;
+
+public class DictionaryConsoleApp {
+
+	public static void main(String[] args) {
+		System.out.println("Dictionary console Application");
+
+	}
+
+}
