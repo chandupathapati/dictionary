67ef6d4116f5e91a2042230af91e599cb2ca3fa3
CP068852 <pchandrashakervarmav@gmail.com>
2021-05-24 22:43:17 -0400

    Update attribute name to differentiate it from class name

--- src/main/java/com/spreetail/dictionary/MultiValueMap.java
+++ src/main/java/com/spreetail/dictionary/MultiValueMap.java
@@ -9,47 +9,47 @@ import java.util.stream.Collectors;
 
 public class MultiValueMap<K, V> {
 
-	private final Map<K, Set<V>> multiValuemap = new HashMap<>();
+	private final Map<K, Set<V>> multimap = new HashMap<>();
 
 	public boolean put(K key, V value) {
-		return this.multiValuemap.computeIfAbsent(key, k -> new HashSet<>()).add(value);
+		return this.multimap.computeIfAbsent(key, k -> new HashSet<>()).add(value);
 	}
 
 	public Set<V> get(K key) {
-		return this.multiValuemap.getOrDefault(key, Collections.emptySet());
+		return this.multimap.getOrDefault(key, Collections.emptySet());
 	}
 
 	public boolean remove(K key, V value) {
-		return this.multiValuemap.get(key).remove(value);
+		return this.multimap.get(key).remove(value);
 	}
 
 	public boolean remove(K key) {
-		this.multiValuemap.remove(key);
-		return !multiValuemap.containsKey(key);
+		this.multimap.remove(key);
+		return !multimap.containsKey(key);
 	}
 
 	public boolean contains(K key, V value) {
-		return this.multiValuemap.getOrDefault(key, Collections.emptySet()).contains(value);
+		return this.multimap.getOrDefault(key, Collections.emptySet()).contains(value);
 	}
 
 	public boolean contains(K key) {
-		return this.multiValuemap.containsKey(key);
+		return this.multimap.containsKey(key);
 	}
 
 	public int size() {
-		return this.multiValuemap.size();
+		return this.multimap.size();
 	}
 
 	public Set<K> keys() {
-		return this.multiValuemap.keySet();
+		return this.multimap.keySet();
 	}
 
 	public Set<V> values() {
-		return multiValuemap.values().stream().flatMap(Set::stream).collect(Collectors.toSet());
+		return multimap.values().stream().flatMap(Set::stream).collect(Collectors.toSet());
 	}
 
 	public boolean clear() {
-		multiValuemap.clear();
-		return multiValuemap.isEmpty();
+		multimap.clear();
+		return multimap.isEmpty();
 	}
 }
